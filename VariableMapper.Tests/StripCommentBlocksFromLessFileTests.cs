79920d1b05ced7a9f82394d8604d38f969c676aa
Zhulien Ivanov <zhulienivanov@yahoo.com>
2017-03-20 22:54:01 +0200

    Add unit test for the less comment strip method.

---
+++ VariableMapper.Tests/StripCommentBlocksFromLessFileTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VariableMapper.Tests
+{
+    [TestClass]
+    public class StripCommentBlocksFromLessFileTests
+    {
+        [TestMethod]
+        public void StripCommentBlocksFromLessFileWorksCorrectly()
+        {
+            var vm = new VariableMapper();
+
+            var filePath = @"..\..\Tests\Inputs\testFile.less";
+            var resultFilePath = @"..\..\Tests\Inputs\testFile-result.less";
+
+            var expectedResult = File.ReadAllText(resultFilePath);
+
+            var result = vm.StripCommentBlocksFromLessFile(filePath);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
